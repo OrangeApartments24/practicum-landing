2a4a85613086de821f317726bfc10a877d191009
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2021-12-18 18:34:10 +0500

    feat: update video link

--- components/Invitement/Invitement.js
+++ components/Invitement/Invitement.js
@@ -5,7 +5,7 @@ const Invitement = () => {
     return(
         <section className={styles.invitement}>
             <video poster='/images/invitement.png' className={styles.video} controls>
-                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type='video/mp4;' />
+                <source src="/invite.mp4" type='video/mp4;' />
             </video>
             <div className={styles.content}>
                 <h2 className={`heading-2 ${styles.heading}`}>Видео-приглашение</h2>
